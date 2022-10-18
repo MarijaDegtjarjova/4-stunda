7742e371fe8780cc26a71e694662da55d61b5f01
marija3 <115552629+MarijaDegtjarjova@users.noreply.github.com>
2022-10-18 12:00:44 +0000

    4

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ if rez:
   print("Trīsstūris eksistē!")
 else:
   print("Trīsstūris neeksistē!")
+
+
+
+skait=0
+while skait<=100:
+  if skait%2!=0:
+    print(skait)
+  skait+=1
+
+
+from math import *
+skait=-5
+while skait<=25:
+  print(cos(skait)**2)
+  skait+=2
