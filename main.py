2441fe2e09bbedf91fb6d544dec9a0d0fa531344
marija3 <115552629+MarijaDegtjarjova@users.noreply.github.com>
2022-10-18 11:07:32 +0000

    +5 stunda

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-skait=0
-while skait<=100:
-  if skait%2!=0:
-    print(skait)
-  skait+=1
+def LenkuParbaude(Len1,Len2,Len3):
+  rezultats = False
+  if Len1+Len2+Len3==180:
+    rezultats = True
+  return rezultats
 
-
-from math import *
-skait=-5
-while skait<=25:
-  print(cos(skait)**2)
-  skait+=2
+Len1 = int(input("Ievadiet 1.leņķi: "))
+Len2 = int(input("Ievadiet 1.leņķi: "))
+Len3 = int(input("Ievadiet 1.leņķi: "))
+rez = LenkuParbaude(Len1,Len2,Len3)
+if rez:
+  print("Trīsstūris eksistē!")
+else:
+  print("Trīsstūris neeksistē!")
